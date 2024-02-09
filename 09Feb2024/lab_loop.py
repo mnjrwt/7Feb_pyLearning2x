643646c8cea34373905df0e80a4782ddccc0a272
mnjrwt <rawat.manoj786@gmail.com>
2024-02-09 08:18:31 +0530

    9 Feb programs

---
+++ 09Feb2024/lab_loop.py
@@ -0,0 +1,17 @@
+# Loop -> Repeat a block of code multiple times
+
+# print("Hello World")
+
+for i in range(1, 10):  # 1-9
+    print("Hello World")
+    print(i)
+
+for i in range(5):  # 0-4
+    print(i)
+
+for i in range(1, 10, 2):  # 1-3-5-7-9
+    print("Hello World")
+    print(i)
+
+# range(start, step, stop)
+
